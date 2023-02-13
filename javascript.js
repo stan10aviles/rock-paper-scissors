6ab220de6abeddb572fdf15fce3e5629bb3bd4bd
Stanley <stan10aviles@hotmail.com>
2023-02-13 15:52:26 -0500

    updated var computerSelection to global scope

--- javascript.js
+++ javascript.js
@@ -6,45 +6,41 @@ function getComputerChoice () {
     let computerOptions = ['rock', 'paper', 'scissors'];
     let computerSelection = computerOptions[Math.floor(Math.random()*computerOptions.length)];
     return computerSelection;
-
+    
 }
 
 let playerSelection = prompt('Lets play rock, paper scissors! Enter your choice below:')
 let computerSelection = getComputerChoice();
 
-//function will take 2 parameters, playerSelection and computerSelection
-//will return string that declares winner "You Lose! Paper beats Rock"
-//Make function's playerSelection case-insensitive (rock, ROCK, RocK)
-
-function playRound (playerSelectionInput, computerSelectionInput) {
+function playRound (playerSelection, computerSelection) {
 
     playerSelection = playerSelection.toLowerCase();
 
-    if (computerSelectionInput === 'paper' && playerSelectionInput === 'rock') {
+    if (computerSelection === 'paper' && playerSelection === 'rock') {
         return 'You Lose! Paper beats Rock.'
     }
-    else if (computerSelectionInput === 'scissors' && playerSelectionInput === 'paper') {
+    else if (computerSelection === 'scissors' && playerSelection === 'paper') {
         return 'You Lose! Scissors beats Paper.'
     }
-    else if (computerSelectionInput === 'paper' && playerSelectionInput === 'scissors') {
+    else if (computerSelection === 'paper' && playerSelection === 'scissors') {
         return 'You win! Scissors beats Paper.'
     }
-    else if (computerSelectionInput === 'scissors' && playerSelectionInput === 'rock') {
+    else if (computerSelection === 'scissors' && playerSelection === 'rock') {
         return 'You win! Rock beats scissors.'
     }
-    else if (computerSelectionInput === 'rock' && playerSelectionInput === 'paper') {
+    else if (computerSelection === 'rock' && playerSelection === 'paper') {
         return 'You win! Paper beats Rock.'
     }
-    else if (computerSelectionInput === 'rock' && playerSelectionInput === 'scissors') {
+    else if (computerSelection === 'rock' && playerSelection === 'scissors') {
         return 'You Lose! Rock beats scissors.'
     }
-    else if (computerSelectionInput === 'rock' && playerSelectionInput === 'rock') {
+    else if (computerSelection === 'rock' && playerSelection === 'rock') {
         return "It's a tie!"
     }
-    else if (    computerSelectionInput === 'paper' && playerSelectionInput === 'paper') {
+    else if (computerSelection === 'paper' && playerSelection === 'paper') {
         return "It's a tie!"
     }
-    else if ( computerSelectionInput === 'scissors' && playerSelectionInput === 'scissors') {
+    else if ( computerSelection === 'scissors' && playerSelection === 'scissors') {
         return "It's a tie!"
     }
     else {
@@ -53,20 +49,6 @@ function playRound (playerSelectionInput, computerSelectionInput) {
 
 }
 
-
-console.log(getComputerChoice());
 console.log(playerSelection);
+console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
-
-
-
-
-
-
-//function playRound (playerSelection, computerSelection) {
-
-//}
-
-//const playerSelection = 'rock';
-//const computerSelection = getComputerChoice();
-//console.log(playRound(playerSelection, computerSelection));
